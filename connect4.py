3d058c1b8f800833b6e84e8ad280739ff897256f
Andreas Siegwarth <andreassiegwarth@startmail.com>
2024-05-05 19:49:21 +0200

    added game mode option to play vs. CPU opponent (who is very low-skill)

--- connect4.py
+++ connect4.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 FIELD_WIDTH = 7
 FIELD_HEIGHT = 6
@@ -6,6 +7,7 @@ UNDERLINE_LENGTH = 21
 
 PLAYER_1_SYMBOL = "X"
 PLAYER_2_SYMBOL = "O"
+BOT_NAME = "Bot"
 MSG_PROMPT_PLAYER_1_FOR_NAME = "Player 1, what is your name?"
 MSG_PROMPT_PLAYER_2_FOR_NAME = "Player 2, what is your name?"
 MSG_THANKS_FOR_PLAYING = "Thanks for playing!"
@@ -15,6 +17,7 @@ MSG_WELCOME = "Welcome, "
 MSG_GOOD_LUCK = "!  And good luck!"
 MSG_LETS_PLAY_CONNECT4 = "Let's play Connect 4!"
 MSG_HAS_WON = " has won!"
+MSG_CHOOSE_MODE = "Select game mode (1 = vs. bot, 2 = two players): "
 ERROR_INVALID_COLUMN = "Error! Invalid column number."
 
 class Player:
@@ -99,28 +102,20 @@ def check_for_win(matrix, symbol):
     check_for_win_diagonal_nw_to_se(matrix, symbol)
     check_for_win_diagonal_sw_to_ne(matrix, symbol)
 
+def check_for_win_vs_bot(matrix, symbol):
+    check_for_win_horizontal(matrix, symbol)
+    check_for_win_vertical(matrix, symbol)
+    check_for_win_diagonal_nw_to_se(matrix, symbol)
+    check_for_win_diagonal_sw_to_ne(matrix, symbol)
+    if game_won == 1:
+        return 1
+
 print(MSG_LETS_PLAY_CONNECT4 + "\n")
 
 def wipe_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
     print_matrix(field)
     print_footer()
-
-wipe_screen()
-
-print(MSG_PROMPT_PLAYER_1_FOR_NAME)
-print(MSG_ENTER_NAME, end = '')
-name = input()
-player1 = Player(name, PLAYER_1_SYMBOL)
-print(MSG_WELCOME + player1.name + MSG_GOOD_LUCK + "\n")
-
-print(MSG_PROMPT_PLAYER_2_FOR_NAME)
-print(MSG_ENTER_NAME, end = '')
-name = input()
-player2 = Player(name, PLAYER_2_SYMBOL)
-print(MSG_WELCOME + player2.name + MSG_GOOD_LUCK + "\n")
-
-active_player = player1
 
 def toggle_active_player():
     global active_player
@@ -129,15 +124,13 @@ def toggle_active_player():
     else:
         active_player = player1
 
-game_won = 0 
-
 def place_token(matrix, column, player):
     for i in range((FIELD_HEIGHT - 1), -1, -1):
         if matrix[i][selected_column] == " ":
             matrix[i][selected_column] = player.token_symbol
             break
 
-def get_column_from_player():
+def get_column_from_player(active_player):
     print(active_player.name + MSG_PROMPT_PLAYER_FOR_MOVE, end = '')
     column_as_string = input()
     try:
@@ -146,21 +139,66 @@ def get_column_from_player():
     except:
         return -1
 
-def prompt_player_for_move():
+def prompt_player_for_move(active_player):
     global selected_column
     while True:
-        selected_column = get_column_from_player()
+        selected_column = get_column_from_player(active_player)
         if selected_column > -1 and selected_column < FIELD_WIDTH:
             break
         else:
             print(ERROR_INVALID_COLUMN)
 
-while game_won != 1:
-    wipe_screen()
-    prompt_player_for_move()
-    place_token(field, selected_column, active_player)
-    print_matrix(field)
-    check_for_win(field, active_player.token_symbol)
-    toggle_active_player()
+wipe_screen()
 
-print(MSG_THANKS_FOR_PLAYING)
+game_won = 0 
+
+print(MSG_PROMPT_PLAYER_1_FOR_NAME)
+print(MSG_ENTER_NAME, end = '')
+name = input()
+player1 = Player(name, PLAYER_1_SYMBOL)
+print(MSG_WELCOME + player1.name + MSG_GOOD_LUCK + "\n")
+print(MSG_CHOOSE_MODE)
+game_mode = input()
+
+def get_random_column_no():
+    return random.randint(0, (FIELD_WIDTH - 1))
+
+if game_mode == "2":
+    print(MSG_PROMPT_PLAYER_2_FOR_NAME)
+    print(MSG_ENTER_NAME, end = '')
+    name = input()
+    player2 = Player(name, PLAYER_2_SYMBOL)
+    print(MSG_WELCOME + player2.name + MSG_GOOD_LUCK + "\n")
+    active_player = player1
+    while game_won != 1:
+        wipe_screen()
+        prompt_player_for_move(active_player)
+        place_token(field, selected_column, active_player)
+        print_matrix(field)
+        check_for_win(field, active_player.token_symbol)
+        toggle_active_player()
+    print(MSG_THANKS_FOR_PLAYING)
+elif game_mode == "1":
+    random.seed()
+    player2 = Player(BOT_NAME, PLAYER_2_SYMBOL)
+    print("You will play against Bot (v0.01)!  Press ENTER to continue.")
+    input()
+    active_player = player1
+    while game_won != 1:
+        wipe_screen()
+        prompt_player_for_move(active_player)
+        place_token(field, selected_column, active_player)
+        print_matrix(field)
+        if 1 == check_for_win_vs_bot(field, active_player.token_symbol):
+            break
+        toggle_active_player()
+        wipe_screen()
+        print("It's Bot's turn now!  Press ENTER to continue.")
+        input()
+        selected_column = get_random_column_no()
+        place_token(field, selected_column, active_player)
+        print_matrix(field)
+        if 0 == check_for_win_vs_bot(field, active_player.token_symbol):
+            break
+        toggle_active_player()
+    print(MSG_THANKS_FOR_PLAYING)
