3a8cab2ca624c8067301b176e4875f52175f85b1
Andreas Siegwarth <andreassiegwarth@startmail.com>
2024-05-05 16:28:29 +0200

    added input validation.  replaced strings with constants.  some other minor things.

--- connect4.py
+++ connect4.py
@@ -4,6 +4,19 @@ FIELD_WIDTH = 7
 FIELD_HEIGHT = 6
 UNDERLINE_LENGTH = 21
 
+PLAYER_1_SYMBOL = "X"
+PLAYER_2_SYMBOL = "O"
+MSG_PROMPT_PLAYER_1_FOR_NAME = "Player 1, what is your name?"
+MSG_PROMPT_PLAYER_2_FOR_NAME = "Player 2, what is your name?"
+MSG_THANKS_FOR_PLAYING = "Thanks for playing!"
+MSG_PROMPT_PLAYER_FOR_MOVE = ", it's your move! Enter the column no. for your token: "
+MSG_ENTER_NAME = "Enter name: "
+MSG_WELCOME = "Welcome, "
+MSG_GOOD_LUCK = "!  And good luck!"
+MSG_LETS_PLAY_CONNECT4 = "Let's play Connect 4!"
+MSG_HAS_WON = " has won!"
+ERROR_INVALID_COLUMN = "Error! Invalid column number."
+
 class Player:
     def __init__(self, name, token_symbol):
         self.name = name
@@ -18,6 +31,12 @@ field = (
     [" ", " ", " ", " ", " ", " ", " "]
 )
 
+def print_matrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i]) - 1):
+            print("["+ matrix[i][j]+ "]", end = '')
+        print("[" + matrix[i][len(matrix[i]) - 1] + "]")
+
 def print_footer():
     for i in range(1, UNDERLINE_LENGTH):
         print("-", end = '')
@@ -26,69 +45,61 @@ def print_footer():
         print(" " + str(i) + " ", end = '')
     print("\n")
 
-def print_matrix(matrix):
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i]) - 1):
-            print("["+ matrix[i][j]+ "]", end = '')
-        print("[" + matrix[i][len(matrix[i]) - 1] + "]")
-
-def check_horizontal(matrix, symbol):
+def check_for_win_horizontal(matrix, symbol):
     global game_won
     for i in range((FIELD_HEIGHT - 1), -1, -1):
-        for j in range(0, 5):
+        for j in range(4):
             if matrix[i][j] == symbol:
                 if matrix[i][j + 1] == symbol:
                     if matrix[i][j + 2] == symbol:
                         if matrix[i][j + 3] == symbol:
-                            print("won! [h]")
+                            print(active_player.name + MSG_HAS_WON)
                             game_won = 1
                             break
 
-def check_vertical(matrix, symbol):
+def check_for_win_vertical(matrix, symbol):
     global game_won
     for i in range((FIELD_HEIGHT - 1), 2, -1):
-        for j in range(0, (FIELD_WIDTH - 1)):
+        for j in range(FIELD_WIDTH - 1):
             if matrix[i][j] == symbol:
                 if matrix[i - 1][j] == symbol:
                     if matrix[i - 2][j] == symbol:
                         if matrix[i - 3][j] == symbol:
-                            print("won! [v]")
+                            print(active_player.name + MSG_HAS_WON)
                             game_won = 1
                             break
 
-
-def check_diagonal_ne_to_sw(matrix, symbol):
+def check_for_win_diagonal_nw_to_se(matrix, symbol):
     global game_won
-    for i in range(0, 4):
-        for j in range(0, 4):
+    for i in range(3):
+        for j in range(4):
             if matrix[i][j] == symbol:
                 if matrix[i + 1][j + 1] == symbol:
                     if matrix[i + 2][j + 2] == symbol:
                         if matrix[i + 3][j + 3] == symbol:
-                            print("won! [ne2sw]")
+                            print(active_player.name + MSG_HAS_WON)
                             game_won = 1
                             break
 
-# diagonal SW to NE
-def check_diagonal_sw_to_ne(matrix, symbol):
+def check_for_win_diagonal_sw_to_ne(matrix, symbol):
     global game_won
     for i in range((FIELD_HEIGHT - 1), 2, -1):
-        for j in range(0, 4):
+        for j in range(4):
             if matrix[i][j] == symbol:
                 if matrix[i - 1][j + 1] == symbol:
                     if matrix[i - 2][j + 2] == symbol:
                         if matrix[i - 3][j + 3] == symbol:
-                            print("won! [sw2ne]")
+                            print(active_player.name + MSG_HAS_WON)
                             game_won = 1
                             break
 
 def check_for_win(matrix, symbol):
-    check_horizontal(matrix, symbol)
-    check_vertical(matrix, symbol)
-    check_diagonal_ne_to_sw(matrix, symbol)
-    check_diagonal_sw_to_ne(matrix, symbol)
+    check_for_win_horizontal(matrix, symbol)
+    check_for_win_vertical(matrix, symbol)
+    check_for_win_diagonal_nw_to_se(matrix, symbol)
+    check_for_win_diagonal_sw_to_ne(matrix, symbol)
 
-print("Let's play Connect 4!\n")
+print(MSG_LETS_PLAY_CONNECT4 + "\n")
 
 def wipe_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -97,17 +108,17 @@ def wipe_screen():
 
 wipe_screen()
 
-print("Player 1, what is your name?")
-print("Enter name: ", end = '')
+print(MSG_PROMPT_PLAYER_1_FOR_NAME)
+print(MSG_ENTER_NAME, end = '')
 name = input()
-player1 = Player(name, "X")
-print("Welcome, " + player1.name + "!  And good luck!\n")
+player1 = Player(name, PLAYER_1_SYMBOL)
+print(MSG_WELCOME + player1.name + MSG_GOOD_LUCK + "\n")
 
-print("Player 2, what is your name?")
-print("Enter name: ", end = '')
+print(MSG_PROMPT_PLAYER_2_FOR_NAME)
+print(MSG_ENTER_NAME, end = '')
 name = input()
-player2 = Player(name, "O")
-print("Welcome, " + player2.name + "!  And good luck!\n")
+player2 = Player(name, PLAYER_2_SYMBOL)
+print(MSG_WELCOME + player2.name + MSG_GOOD_LUCK + "\n")
 
 active_player = player1
 
@@ -121,23 +132,35 @@ def toggle_active_player():
 game_won = 0 
 
 def place_token(matrix, column, player):
-    if matrix[0][selected_column] != " ":
-        print("Column is full!")
-    else:
-        for i in range((FIELD_HEIGHT - 1), -1, -1):
-            if matrix[i][selected_column] == " ":
-                matrix[i][selected_column] = player.token_symbol
-                break
+    for i in range((FIELD_HEIGHT - 1), -1, -1):
+        if matrix[i][selected_column] == " ":
+            matrix[i][selected_column] = player.token_symbol
+            break
 
-toggle_active_player()
+def get_column_from_player():
+    print(active_player.name + MSG_PROMPT_PLAYER_FOR_MOVE, end = '')
+    column_as_string = input()
+    try:
+        column_as_int = int(column_as_string) - 1
+        return column_as_int
+    except:
+        return -1
+
+def prompt_player_for_move():
+    global selected_column
+    while True:
+        selected_column = get_column_from_player()
+        if selected_column > -1 and selected_column < FIELD_WIDTH:
+            break
+        else:
+            print(ERROR_INVALID_COLUMN)
 
 while game_won != 1:
     wipe_screen()
-    toggle_active_player()
-    print(active_player.name + ", it's your move! Enter the column no. for your token: ", end = '')
-    selected_column = int(input()) - 1
+    prompt_player_for_move()
     place_token(field, selected_column, active_player)
     print_matrix(field)
     check_for_win(field, active_player.token_symbol)
+    toggle_active_player()
 
-print("Thanks for playing!")
+print(MSG_THANKS_FOR_PLAYING)
